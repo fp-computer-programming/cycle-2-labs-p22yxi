35efe251cbee7c8ef3c2434f36393e821a58f27c
p22yxi <p22yxi@fairfieldprep.org>
2021-09-20 10:03:55 -0400

    1

---
+++ lab_5-1.py
@@ -0,0 +1,23 @@
+# Author : Yongdong Xi (Sep 20 2021)
+
+# quesrtion 1
+# x += 5
+x = x + 5
+
+# question 2
+y -= 3
+
+# question 3
+z = z ** 2
+
+# question 4
+w %= 2
+
+# question 5
+t /= 5
+
+# question 6
+a = a * 12
+
+# question 7
+i //= 7
